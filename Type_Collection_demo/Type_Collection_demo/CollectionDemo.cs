0f0c63150f110c2282cbc8deb6bb18ac8e85efc8
sunil429 <68459801+sunil429@users.noreply.github.com>
2021-10-31 18:27:54 +0530

    Add files via upload

---
+++ Type_Collection_demo/Type_Collection_demo/CollectionDemo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Type_Collection_demo
+{
+    class CollectionDemo
+    {
+        static void Main(string[] args)
+        {
+            #region Nullable Type
+
+            //int i = null;
+            Nullable<int> x = null;
+            x = 100;
+            x = null;
+
+            //datatype?  ---------- nullable type
+            int? y = null;
+            #endregion
+
+            // #region Generic Collection classes
+            // List<string> countryList = new List<string>();
+
+            // List<int> numbers = new List<int>();
+            // numbers.Add(100);
+
+            // List<Person> personlList = new List<Person>();
+            // personlList.Add(new Person() { FirstName = "Sunil", LastName = "Pillai" });
+            // Console.WriteLine(personlList[0].FirstName);
+            // #endregion
+
+            //  #region System.Collections.classes
+
+            //  ArrayList countryList = new ArrayList();
+            // countryList.Add("India");
+            // countryList.Add("USA");
+            // countryList.Insert(1, "France");
+
+            // for(int i = 0; i < countryList.Count; i++)
+            //{
+            //    Console.WriteLine(countryList[i]);
+            // }
+            //Person person = new Person() { FirstName = "Sunil", LastName = "Pillai" };
+            //Console.WriteLine(person.FirstName + "" + person.LastName);
+            // Console.WriteLine("---------------------------\n");
+            //countryList.Add(person);
+
+            //Console.WriteLine(((Person)countryList[3]).FirstName);
+            // Console.WriteLine("---------------------------\n");
+
+            // #endregion
+            //int[] numberArray = new int[3];
+            // numberArray[0] = 454;
+            // numberArray[1] = 44754;
+            // numberArray[2] = 23;
+
+            //for(int i = 0; i < numberArray.Length; i++)
+            // {
+            //Console.WriteLine(numberArray[i]);
+            // }
+
+        }
+    }
+}
---
+++ Type_Collection_demo/Type_Collection_demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Type_Collection_demo
+{
+    struct Employee
+    {
+        public int EmpNo;
+        public string EmpName;
+        public double Salary;
+
+        public double AnnualSalary()
+        {
+            return Salary * 12;
+        }
+        public Employee(int eno,string name,double amount)
+        {
+            this.EmpNo = eno;
+            this.EmpName = name;
+            this.Salary = amount;
+        }
+        public override string ToString()
+        {
+            return string.Format($"Empno { EmpNo} Employee Name :{ EmpName} Salary :{Salary}");
+        
+        }
+    }
+    enum Month
+    {
+        jan = 1, feb, mar, apr,may, jun,jul, aug, sep, oct, nov, dec
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(101, "sunil", 10000);
+            Console.WriteLine("Annual Salary" + employee.AnnualSalary());
+            Console.WriteLine(employee);
+
+
+            Console.WriteLine(DayOfWeek.Wednesday + ":" + (int)DayOfWeek.Wednesday);
+            Console.WriteLine(Month.oct + ":" + (int)Month.oct);
+
+
+        }
+    }
+}
