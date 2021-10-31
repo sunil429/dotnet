8b9aa097aaa2ddb81414d2dd5b6469334f1f5d22
sunil429 <68459801+sunil429@users.noreply.github.com>
2021-10-31 18:30:46 +0530

    Add files via upload

---
+++ OOPSDemo/BankLibrary/Bank.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace BankLibrary
+{
+    public class Bank
+    {
+
+        #region Fields & Properties
+
+        private string accHolderName;
+
+        public string AccHolderName
+        {
+            get { return accHolderName; }
+            set { accHolderName = value; }
+        }
+
+        private double balance;
+
+        public double Balance
+        {
+            get { return balance; }
+            protected set { balance = value; }
+
+        }
+
+        #endregion
+        #region Methods 
+        public void Deposit(double amount )
+        {
+            Balance += amount;
+        }
+        public virtual  void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public override string ToString()
+        {   //traditional
+            // return "AccountHolderName : " + AccHolderName + "Balance" + Balance;
+
+            //formatted string
+            //return string.Format("AccountHolderName : {0} Balance : {1}", AccHolderName , Balance);
+
+            //string interpolation
+            return string.Format($"Account Holder Name : {AccHolderName} Balance :{Balance}");
+        }
+        #endregion
+        #region Constructor
+
+        public Bank()
+        {
+            Balance = 1000;
+        }
+        public Bank(string name,double amount)
+        {
+            this.AccHolderName = name;
+            this.Balance = amount;
+        }
+        #endregion
+    }
+}
---
+++ OOPSDemo/BankLibrary/Savings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class Savings : Bank , IInterest
+    {
+
+        public Savings()
+        { }
+        public Savings(string name, double amt)
+        { }
+        public double CalculateInterest()
+        {
+           return Balance * .1;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if ((Balance - amount) > 0)
+            {
+                Balance -= amount;
+            }
+
+            else
+            {
+                throw new BalanceException("Balance not enough ");
+            }
+        }
+    }
+}
