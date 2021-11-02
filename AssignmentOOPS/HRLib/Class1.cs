b1403a999789961bf917aab5d51a876be3fb8de8
sunil429 <68459801+sunil429@users.noreply.github.com>
2021-11-02 19:16:54 +0530

    Add files via upload

---
+++ AssignmentOOPS/HRLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HRLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ AssignmentOOPS/HRLib/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLib
+{
+  public  class Employee
+    {
+
+        #region Fields & Properties
+        string empName;
+        public string EmpName
+        {
+            get { return empName; }
+            set { empName = value; }
+        }
+        string address;
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        #endregion
+
+        #region constructor
+        public Employee()
+        {
+            empName = "sunil";
+            address = "mumbai";
+
+        }
+        public Employee(string name , string add)
+        {
+            this.EmpName = name;
+            this.Address = add;
+        }
+
+        #endregion
+        #region method
+        public override string ToString()
+        {
+            return string.Format($"Employee Name : {EmpName} Address :{Address}");
+        }
+
+        #endregion
+    }
+}
