fa3567a34ab971a3aa2d68160dbbc06578f29549
sunil429 <68459801+sunil429@users.noreply.github.com>
2021-12-06 13:29:13 +0530

    Add files via upload

---
+++ Server/Controllers/EmployeesController.cs
@@ -0,0 +1,39 @@
+﻿using BlazorEmployeeDemo.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmployeeDemo.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private static List<EmployeeModel> empList = new List<EmployeeModel>();
+
+        public EmployeesController()
+        {
+            //populate empList with records
+            if (empList.Count == 0)
+            {
+                empList.Add(new EmployeeModel { EmpNo = 1, EmpName = "Bhavana", HireDate = Convert.ToDateTime("01/01/2001"), Salary = 100000 });
+                empList.Add(new EmployeeModel { EmpNo = 2, EmpName = "Vishal", HireDate = Convert.ToDateTime("12/12/2012"), Salary = 500000 });
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<EmployeeModel> GetAllEmps()
+        {
+            return empList.ToArray();
+        }
+
+        [HttpPost]
+        public void PostEmp(EmployeeModel employee)
+        {
+            empList.Add(employee);
+        }
+    }
+}
---
+++ Shared/EmployeeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEmployeeDemo.Shared
+{
+   public   class EmployeeModel
+    {
+        [Required]
+        public int? EmpNo { get; set; }
+
+        [Required]
+        public string EmpName { get; set; }
+
+        [Required]
+        public DateTime? HireDate { get; set; }
+        [Required]
+        public double? Salary { get; set; }
+    }
+}
